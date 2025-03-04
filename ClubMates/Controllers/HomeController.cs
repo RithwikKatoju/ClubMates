ecebf9383b9ab7884d83c50924d5d2cf3f574ad7
Katoju Rithwik <2388305@cognizant.com>
2025-03-04 14:49:15 +0530

    DAY0

---
+++ ClubMates/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClubMates.Models;
+
+namespace ClubMates.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Student student = new Student()
+            {
+                StudentID = 1,
+                StudentName = "Rithwik",
+                DateOfBirth = new DateTime(2004, 05, 31),
+                Height = 179,
+                Weight = 70
+            };
+
+            List<Student> studnets = new List<Student>()
+            {
+                new Student() { StudentID = 1, StudentName = 
+            }
+            return View(student);
+        }
+    }
+}
